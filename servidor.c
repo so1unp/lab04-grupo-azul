b0f29e768f266a369c375f8d5a196513fbbde208
SolaNicolas <nicolas.solaleiva@gmail.com>
2026-06-08 19:54:04 -0300

    Memoria Compartida mapa, shm/servidor_map_shm

--- servidor.c
+++ servidor.c
@@ -9,11 +9,15 @@
 #include <unistd.h>
 #include <time.h>
 
-#define WIN_WIDTH 134
-#define WIN_HEIGHT 25
+#include <sys/mman.h>
+#include <sys/stat.h>
+
+#define WIN_WIDTH 90
+#define WIN_HEIGHT 30
 #define BUFF_SIZE 1024
 
 #define RECEIVER_MESSAGE_QUEUE "/servidor_receiver"
+#define SHM_MAP_PATH "/servidor_map_shm"
 
 typedef struct {
     WINDOW *win;
@@ -21,7 +25,13 @@ typedef struct {
     char *buff;
 } ReceiverData;
 
+typedef struct {
+    WINDOW *win;
+    char (*map)[WIN_WIDTH];
+} MapData;
+
 void *receive_mq(void *param);
+void *print_map(void *param);
 
 int main(int argc, char *argv[])
 {
@@ -29,11 +39,38 @@ int main(int argc, char *argv[])
 
     WINDOW *win;
     pthread_t t_receiver;
+    pthread_t t_printer;
 
-    char map[WIN_HEIGHT][WIN_WIDTH];
     char buff[BUFF_SIZE];
     char input[BUFF_SIZE];
 
+    int shm_fd;
+    size_t map_size = WIN_HEIGHT * WIN_WIDTH;
+    char (*map)[WIN_WIDTH];
+
+    shm_fd = shm_open(SHM_MAP_PATH, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    if (shm_fd < 0) {
+        perror("Error al crear la memoria compartida");
+        exit(1);
+    }
+
+    if (ftruncate(shm_fd, (off_t)map_size) == -1) {
+        perror("Error en ftruncate");
+        close(shm_fd);
+        shm_unlink(SHM_MAP_PATH);
+        exit(1);
+    }
+
+    map = mmap(NULL, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (map == MAP_FAILED) {
+        perror("Error en mmap");
+        close(shm_fd);
+        shm_unlink(SHM_MAP_PATH);
+        exit(1);
+    }
+
+    memset(map, ' ', map_size);
+    
     mqd_t receiver;
     if ((receiver = mq_open (RECEIVER_MESSAGE_QUEUE,  O_RDWR)) == -1) { 
         printf("No se puede acceder a la cola de mensajes %s", RECEIVER_MESSAGE_QUEUE); 
@@ -48,7 +85,7 @@ int main(int argc, char *argv[])
     refresh();
 
     /* Inicializa Ventana */
-    win = newwin(WIN_HEIGHT, WIN_WIDTH, 0, 1);
+    win = newwin(WIN_HEIGHT, WIN_WIDTH, 0, 0);
     box(win, 0, 0);
     wrefresh(win);
 
@@ -58,7 +95,13 @@ int main(int argc, char *argv[])
         .buff = buff
     };
 
+    MapData map_data = {
+        .win = win,
+        .map = map
+    };
+
     pthread_create(&t_receiver, NULL, receive_mq, (void *)&receiver_data);
+    pthread_create(&t_printer, NULL, print_map, (void *)&map_data);
 
     /* Bucle */
     while(strcmp(input, "FIN") != 0) {
@@ -71,8 +114,13 @@ int main(int argc, char *argv[])
     werase(win);
     endwin();
     pthread_join(t_receiver, NULL);
-    
+    pthread_join(t_printer, NULL);
+
     mq_close(receiver);
+
+    munmap(map, map_size);
+    close(shm_fd);
+    shm_unlink(SHM_MAP_PATH);
 
     exit(EXIT_SUCCESS);
 }
@@ -83,6 +131,8 @@ void *receive_mq(void *param) {
     static int row = 1;
 
     while(1) {
+        memset(data->buff, 0, BUFF_SIZE);
+
         if (mq_receive(data->receiver, data->buff, BUFF_SIZE, &prio) == -1) {
             perror("Error al recibir el mensaje");
             return NULL;
@@ -97,5 +147,23 @@ void *receive_mq(void *param) {
         if (row > WIN_HEIGHT - 2) row = 1;
     }
     
+    return NULL;
+}
+
+void *print_map(void *param) {
+    MapData *data = (MapData *)param;
+
+    while(1) {
+        for (int i = 0; i < WIN_HEIGHT; i++) {
+            for (int j = 0; j < WIN_WIDTH; j++) {
+                wmove(data->win, i, j);
+                wprintw(data->win, "%c", data->map[i][j]);
+            }
+        }
+        box(data->win, 0, 0);
+        wrefresh(data->win);
+        sleep(1);
+    }
+
     return NULL;
 }
