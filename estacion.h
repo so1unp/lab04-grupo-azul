6f48055c41728498464f43dba4903a65cde7cd17
Enzo <enzoriera987@gmail.com>
2026-06-20 18:06:23 -0300

    posible choque inicial con asteroide evitado

--- estacion.h
+++ estacion.h
@@ -2,6 +2,18 @@
 #define ESTACION_H
 
 #include <ncurses.h>
+#define NUM_RECURSOS 4
+
+/* Indices de los recursos dentro del array de cargamento */
+#define IDX_DEUTERIO    0   // es el combustible que consume la nave
+#define IDX_MUTEXIO     1
+#define IDX_SEMAFORITA  2
+#define IDX_KERNELIO    3
+
+
+#define COMBUSTIBLE_INICIAL  0
+#define OXIGENO_INICIAL      100
+#define CONSUMO_OXIGENO      1
 
 typedef struct {
     int  id;
@@ -9,6 +21,10 @@ typedef struct {
     int  y;
     int  combustible;
     int  oxigeno;
+    int  cargamento[NUM_RECURSOS];
+    int  activa;
+    char simbolo;
+    pthread_mutex_t mutex;  /* un mutex por estacion */
 } Estacion;
 
 #endif
